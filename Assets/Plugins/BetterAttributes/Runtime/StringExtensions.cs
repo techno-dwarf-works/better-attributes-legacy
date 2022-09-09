3593f6e10ae5e39ed0904ee42f83553cfc0a085b
uurha <uurh2man@gmail.com>
2022-09-09 16:11:25 +0200

    Hide internal methods

--- Assets/Plugins/BetterAttributes/Runtime/StringExtensions.cs
+++ Assets/Plugins/BetterAttributes/Runtime/StringExtensions.cs
@@ -4,12 +4,12 @@ namespace BetterAttributes.Runtime
 {
     public static class StringExtensions
     {
-        public static string PrettyCamelCase(this string input)
+        internal static string PrettyCamelCase(this string input)
         {
             return Regex.Replace(input.Replace("_", ""), "((?<!^)([A-Z][a-z]|(?<=[a-z])[A-Z]))", " $1").Trim();
         }
 
-        public static string ToTitleCase(this string input)
+        internal static string ToTitleCase(this string input)
         {
             return Regex.Replace(input, @"(^\w)|(\s\w)", m => m.Value.ToUpper());
         }
