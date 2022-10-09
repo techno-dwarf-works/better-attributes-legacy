2ca2d61770f59b3a0c1a5f6e65c580f11e9cc55a
uurha <uurh2man@gmail.com>
2022-10-09 23:18:28 +0200

    Update

--- Assets/Plugins/BetterAttributes/Editor/EditorAddons/Drawers/Select/SelectImplementationDrawer.cs
+++ Assets/Plugins/BetterAttributes/Editor/EditorAddons/Drawers/Select/SelectImplementationDrawer.cs
@@ -1,175 +1,20 @@
-﻿#region license
-
-// Copyright 2021 - 2022 Arcueid Elizabeth D'athemon
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//     http://www.apache.org/licenses/LICENSE-2.0
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
-
-#endregion
-
-using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using System;
 using BetterAttributes.Runtime.Attributes.Select;
 using UnityEditor;
-using UnityEngine;
-using Object = UnityEngine.Object;
 
 namespace BetterAttributes.EditorAddons.Drawers.Select
 {
     [CustomPropertyDrawer(typeof(SelectImplementationAttribute))]
-    public class SelectImplementationDrawer : SelectDrawerBase<SelectImplementationAttribute>
+    public class SelectImplementationDrawer : SelectTypeDrawer
     {
-        private Type _type;
-        private List<object> _reflectionTypes;
-
-        private void LazyGetAllInheritedType(Type baseType, Type currentObjectType)
-        {
-            if (_reflectionTypes != null) return;
-
-            _reflectionTypes = AppDomain.CurrentDomain.GetAssemblies()
-                .SelectMany(s => s.GetTypes()).Where(p => ArgIsValueType(baseType, currentObjectType, p))
-                .Select(x => (object)x).ToList();
-            _reflectionTypes.Insert(0, null);
-        }
-
-        private bool ArgIsValueType(Type baseType, Type currentObjectType, Type iterateValue)
-        {
-            return iterateValue != currentObjectType && CheckType(baseType, iterateValue) &&
-                   (iterateValue.IsClass || iterateValue.IsValueType) &&
-                   !iterateValue.IsAbstract && !iterateValue.IsSubclassOf(typeof(Object));
-        }
-
-        private bool CheckType(Type baseType, Type p)
-        {
-            return baseType.IsAssignableFrom(p);
-        }
-
-        private protected override bool CheckSupported(SerializedProperty property)
-        {
-            return property.propertyType == SerializedPropertyType.ManagedReference;
-        }
-
-        private protected override GUIContent GenerateHeader()
-        {
-            return new GUIContent("Available Types");
-        }
-
         private protected override object GetCurrentValue(SerializedProperty property)
         {
             return property.managedReferenceValue;
         }
 
-        private protected override string GetButtonName(object currentValue)
-        {
-            return currentValue == null ? "null" : currentValue.GetType().Name;
-        }
-
-        private protected override void Setup(SerializedProperty property, SelectImplementationAttribute currentAttribute)
-        {
-            var currentObjectType = property.serializedObject.targetObject.GetType();
-            LazyGetAllInheritedType(currentAttribute.GetFieldType() ?? fieldInfo.FieldType, currentObjectType);
-        }
-
         private protected override void UpdateValue(SerializedProperty property)
         {
             property.managedReferenceValue = _type == null ? null : Activator.CreateInstance(_type);
-        }
-
-        private protected override string[] ResolveGroupedName(object value, DisplayGrouping grouping)
-        {
-            if (value == null)
-            {
-                return new string[] { "null" };
-            }
-
-            if(value is Type type)
-            {
-                if (string.IsNullOrEmpty(type.FullName))
-                {
-                    return new string[] { type.Name };
-                }
-
-                var split = type.FullName.Split('.');
-                if (split.Length <= 1)
-                {
-                    return new string[] { type.Name };
-                }
-
-                switch (grouping)
-                {
-                    case DisplayGrouping.Grouped:
-                        return split;
-                    case DisplayGrouping.GroupedFlat:
-                        return new string[] { split.First(), split.Last() };
-                    default:
-                        throw new ArgumentOutOfRangeException(nameof(grouping), grouping, null);
-                }
-            }
-
-            return new string[] { NotSupported };
-        }
-
-        private protected override List<object> GetSelectCollection()
-        {
-            return _reflectionTypes;
-        }
-
-        private protected override string ResolveName(object value, DisplayName displayName)
-        {
-            if (value == null)
-            {
-                return "null";
-            }
-
-            if (value is Type type)
-            {
-                switch (displayName)
-                {
-                    case DisplayName.Short:
-                        return $"{type.Name}";
-                    case DisplayName.Full:
-                        return $"{type.FullName}";
-                    default:
-                        throw new ArgumentOutOfRangeException(nameof(displayName), displayName, null);
-                }
-            }
-
-            return NotSupported;
-        }
-
-        private protected override bool ResolveState(object currentValue, object iteratedValue)
-        {
-            if (iteratedValue == null && currentValue == null) return true;
-            return iteratedValue is Type type && currentValue?.GetType() == type;
-        }
-
-        private protected override void OnSelectItem(object obj)
-        {
-            if (obj == null)
-            {
-                _type = null;
-                SetNeedUpdate();
-                return;
-            }
-
-            var type = (Type)obj;
-            if (_type != null)
-            {
-                if (_type == type)
-                {
-                    return;
-                }
-            }
-
-            _type = type;
-            SetNeedUpdate();
         }
     }
 }
---
+++ Assets/Plugins/BetterAttributes/Editor/EditorAddons/Drawers/Select/SelectTypeDrawer.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using BetterAttributes.Runtime.Attributes.Select;
+using UnityEditor;
+using UnityEngine;
+using Object = UnityEngine.Object;
+
+namespace BetterAttributes.EditorAddons.Drawers.Select
+{
+    public abstract class SelectTypeDrawer : SelectDrawerBase<SelectImplementationAttribute>
+    {
+        private protected Type _type;
+        private List<object> _reflectionTypes;
+
+        private void LazyGetAllInheritedType(Type baseType, Type currentObjectType)
+        {
+            if (_reflectionTypes != null) return;
+
+            _reflectionTypes = AppDomain.CurrentDomain.GetAssemblies()
+                .SelectMany(s => s.GetTypes()).Where(p => ArgIsValueType(baseType, currentObjectType, p))
+                .Select(x => (object)x).ToList();
+            _reflectionTypes.Insert(0, null);
+        }
+
+        private bool ArgIsValueType(Type baseType, Type currentObjectType, Type iterateValue)
+        {
+            return iterateValue != currentObjectType && CheckType(baseType, iterateValue) &&
+                   (iterateValue.IsClass || iterateValue.IsValueType) &&
+                   !iterateValue.IsAbstract && !iterateValue.IsSubclassOf(typeof(Object));
+        }
+
+        private bool CheckType(Type baseType, Type p)
+        {
+            return baseType.IsAssignableFrom(p);
+        }
+
+        private protected override bool CheckSupported(SerializedProperty property)
+        {
+            return property.propertyType == SerializedPropertyType.ManagedReference;
+        }
+
+        private protected override GUIContent GenerateHeader()
+        {
+            return new GUIContent("Available Types");
+        }
+
+        private protected override string GetButtonName(object currentValue)
+        {
+            return currentValue == null ? "null" : currentValue.GetType().Name;
+        }
+
+        private protected override void Setup(SerializedProperty property, SelectImplementationAttribute currentAttribute)
+        {
+            var currentObjectType = property.serializedObject.targetObject.GetType();
+            LazyGetAllInheritedType(currentAttribute.GetFieldType() ?? fieldInfo.FieldType, currentObjectType);
+        }
+
+        private protected override string[] ResolveGroupedName(object value, DisplayGrouping grouping)
+        {
+            if (value == null)
+            {
+                return new string[] { "null" };
+            }
+
+            if(value is Type type)
+            {
+                if (string.IsNullOrEmpty(type.FullName))
+                {
+                    return new string[] { type.Name };
+                }
+
+                var split = type.FullName.Split('.');
+                if (split.Length <= 1)
+                {
+                    return new string[] { type.Name };
+                }
+
+                switch (grouping)
+                {
+                    case DisplayGrouping.Grouped:
+                        return split;
+                    case DisplayGrouping.GroupedFlat:
+                        return new string[] { split.First(), split.Last() };
+                    default:
+                        throw new ArgumentOutOfRangeException(nameof(grouping), grouping, null);
+                }
+            }
+
+            return new string[] { NotSupported };
+        }
+
+        private protected override List<object> GetSelectCollection()
+        {
+            return _reflectionTypes;
+        }
+
+        private protected override string ResolveName(object value, DisplayName displayName)
+        {
+            if (value == null)
+            {
+                return "null";
+            }
+
+            if (value is Type type)
+            {
+                switch (displayName)
+                {
+                    case DisplayName.Short:
+                        return $"{type.Name}";
+                    case DisplayName.Full:
+                        return $"{type.FullName}";
+                    default:
+                        throw new ArgumentOutOfRangeException(nameof(displayName), displayName, null);
+                }
+            }
+
+            return NotSupported;
+        }
+
+        private protected override bool ResolveState(object currentValue, object iteratedValue)
+        {
+            if (iteratedValue == null && currentValue == null) return true;
+            return iteratedValue is Type type && currentValue?.GetType() == type;
+        }
+
+        private protected override void OnSelectItem(object obj)
+        {
+            if (obj == null)
+            {
+                _type = null;
+                SetNeedUpdate();
+                return;
+            }
+
+            var type = (Type)obj;
+            if (_type != null)
+            {
+                if (_type == type)
+                {
+                    return;
+                }
+            }
+
+            _type = type;
+            SetNeedUpdate();
+        }
+    }
+}
