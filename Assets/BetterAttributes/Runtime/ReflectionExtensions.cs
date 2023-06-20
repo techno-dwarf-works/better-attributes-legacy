f1808d445a6072368b57de5bec4fecdafd74f96f
uurhaa <uurha.dev@gmail.com>
2023-06-20 02:16:40 +0200

    Update BetterEditorTools package

--- Assets/BetterAttributes/Runtime/ReflectionExtensions.cs
+++ Assets/BetterAttributes/Runtime/ReflectionExtensions.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
-using Better.EditorTools.Runtime;
+using Better.Tools.Runtime;
 
 namespace Better.Attributes.Runtime
 {
